a8ab840e613b4e29cbace2225b7c7b98ad8cafed
nikitaasainii <nikitasaini0912@gmail.com>
2026-01-30 00:45:05 +0530

    Update import statement for AdamW and scheduler

--- scripts/train.py
+++ scripts/train.py
@@ -25,7 +25,7 @@ from deepfake_detector.data import create_combined_dataset, get_train_transforms
 from deepfake_detector.utils import setup_logger, calculate_comprehensive_metrics, plot_confusion_matrix, plot_training_history
 from deepfake_detector.config import Config, load_config
 
-from transformers import AdamW, get_cosine_schedule_with_warmup
+from transformers.optimization import AdamW, get_cosine_schedule_with_warmup
 
 
 def train_epoch(model, dataloader, criterion, optimizer, scheduler, device, epoch, logger):
