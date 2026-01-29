8cd16ee589ac370b7259caa9a46148e2a1cb1acf
nikitaasainii <nikitasaini0912@gmail.com>
2026-01-30 00:14:49 +0530

    Import create_combined_dataset in __init__.py

--- deepfake_detector/data/__init__.py
+++ deepfake_detector/data/__init__.py
@@ -3,7 +3,7 @@
 from deepfake_detector.data.dataset import DeepFakeDataset
 from deepfake_detector.data.transforms import get_train_transforms, get_val_transforms
 from deepfake_detector.data.loader import create_dataloaders
-
+from .dataset import create_combined_dataset
 __all__ = [
     "DeepFakeDataset",
     "get_train_transforms",
